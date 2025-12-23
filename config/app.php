9a0f7c5b1fa971e7ef0f0fc7b72c15410c12e042
Edwin Choque <edwin@gmail.com>
2025-12-22 23:51:48 -0400

    Se puso el sistema en idioma español

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'America/La_Paz',
 
     /*
     |--------------------------------------------------------------------------
@@ -78,11 +78,24 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'en'),
+    /*'locale' => env('APP_LOCALE', 'en'),
 
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
-    'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
+    'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),*/
+
+
+    'locale' => 'es',
+
+    // env('APP_FALLBACK_LOCALE', 'en')
+
+    'fallback_locale' => 'es',
+
+    // env('APP_FAKER_LOCALE', 'en_US')
+
+    'faker_locale' => 'es_ES',
+
+
 
     /*
     |--------------------------------------------------------------------------
